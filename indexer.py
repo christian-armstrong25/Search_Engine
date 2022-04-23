159e596f70a0f5b159888e924512ffab07c11579
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 18:36:08 -0400

    hello

--- indexer.py
+++ indexer.py
@@ -18,18 +18,18 @@ class Indexer:
         wiki_xml_root = wiki_tree.getroot()
 
         for wiki_page in wiki_xml_root:
-            self.text_set.add(wiki_page.find('title').text.strip())
+            self.text_set.add(wiki_page.find('title').text.strip().lower())
             
             self.id_to_title.update(int(wiki_page.find('id').text.strip()), wiki_page.find('title').text.strip())
 
-            self.text_set.add(wiki_page.find('text').text.strip())
+            self.text_set.add(wiki_page.find('text').text.strip().lower())
             
     def tokenize(self) -> None:
         link_regex = '''\[\[[^\[]+?\]\]'''
         text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
 
         for string in self.text_set:
-            text = re.findall(text_regex, string.lower())
+            text = re.findall(text_regex, string)
             for word in text:
 
 
