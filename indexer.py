737ea7cae24a196ed867b07513816244ace967e6
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 19:32:06 -0400

    Done with parsing we think

--- indexer.py
+++ indexer.py
@@ -5,41 +5,46 @@ from typing import Set
 import xml.etree.ElementTree as et
 import re
 
+from file_io import write_title_file
+
 class Indexer:
     def __init__(self) -> None:
         if len(sys.argv) - 1 != 4:
             print("Fewer than four arguments!")
         else:
-            self.text_set = set()
+            self.corpus = set()
             self.id_to_title = {}
             self.link_to_links = {}
             self.parse(sys.argv[1])
+            write_title_file(sys.argv[2], self.id_to_title)
 
     def parse(self, input_file : String) -> None:
         wiki_tree = et.parse(input_file)
         wiki_xml_root = wiki_tree.getroot()
 
         for wiki_page in wiki_xml_root:
-            self.text_set.add(wiki_page.find('title').text.strip().lower())
+            for word in wiki_page.find('title').text.strip().lower().split():
+                self.corpus.add(word)
             
             self.id_to_title.update(int(wiki_page.find('id').text.strip()), wiki_page.find('title').text.strip())
 
-            self.text_set.add(wiki_page.find('text').text.strip().lower())
+            for word in wiki_page.find('text').text.strip().lower().split():
+                self.corpus.add(wiki_page.find('text').text.strip().lower())
             
     def tokenize(self) -> None:
         link_regex = '''\[\[[^\[]+?\]\]'''
         text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
 
-        for string in self.text_set:
+        for string in self.corpus:
             text = re.findall(text_regex, string)
             links = re.findall(link_regex, string)
             for link in links:
                 split_link = link.split('|')
                 if len(split_link) == 1:
-                    self.text_set.add(split_link[0])
+                    self.corpus.add(split_link[0])
                     self.link_to_links.update(split_link[0], )
                 elif len(split_link) == 2:
-                    self.text_set.add(split_link[1])
+                    self.corpus.add(split_link[1])
                     self.link_to_links.add(split_link[0])
 
                     
