c85c507bb784f5e25da5f13c31ccd044d591150b
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 17:30:36 -0400

    Added stuff

--- indexer.py
+++ indexer.py
@@ -1,4 +1,13 @@
+from msilib.schema import File
 import sys
 import xml.etree.ElementTree as et
 
-print("hi")
+class Indexer:
+    def __init__(self) -> None:
+        pass
+
+    def parse(input_file : String) -> None:
+        wiki_tree = et.parse(input_file)
+        wiki_xml_root = wiki_tree.getroot()
+        for wiki_page in wiki_xml_root:
+            
