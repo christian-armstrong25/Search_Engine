e4c0179f98425aa9ef7cefba41154cfc7c455e8d
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 18:31:05 -0400

    trying tokenize

--- indexer.py
+++ indexer.py
@@ -2,6 +2,7 @@ import sys
 from tokenize import String
 from typing import Set
 import xml.etree.ElementTree as et
+import re
 
 class Indexer:
     def __init__(self) -> None:
@@ -9,6 +10,7 @@ class Indexer:
             print("Fewer than four arguments!")
         else:
             self.text_set = set()
+            self.id_to_title = {}
             self.parse(sys.argv[1])
 
     def parse(self, input_file : String) -> None:
@@ -18,16 +20,18 @@ class Indexer:
         for wiki_page in wiki_xml_root:
             self.text_set.add(wiki_page.find('title').text.strip())
             
-            self.text_set.add(int(wiki_page.find('id').text.strip()))
+            self.id_to_title.update(int(wiki_page.find('id').text.strip()), wiki_page.find('title').text.strip())
 
             self.text_set.add(wiki_page.find('text').text.strip())
             
-    def tokenize(self, text_set : Set) -> None:
+    def tokenize(self) -> None:
         link_regex = '''\[\[[^\[]+?\]\]'''
         text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
 
-        for 
-        cool_token = re.findall(n_regex, "this is a cool string")
+        for string in self.text_set:
+            text = re.findall(text_regex, string.lower())
+            for word in text:
+
 
 
         
