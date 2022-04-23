2684c425d1ba72b3237bbddd7629a73e9aed9419
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 18:15:35 -0400

    More parsing

--- indexer.py
+++ indexer.py
@@ -1,26 +1,34 @@
-from msilib.schema import File
 import sys
+from tokenize import String
+from typing import Set
 import xml.etree.ElementTree as et
 
 class Indexer:
     def __init__(self) -> None:
-        pass
+        if len(sys.argv) - 1 != 4:
+            print("Fewer than four arguments!")
+        else:
+            self.text_set = set()
+            self.parse(sys.argv[1])
 
-    def parse(input_file : String) -> :
+    def parse(self, input_file : String) -> None:
         wiki_tree = et.parse(input_file)
         wiki_xml_root = wiki_tree.getroot()
 
-        text_set = set()
         for wiki_page in wiki_xml_root:
-            title = wiki_page.find('title').text.strip()
-            if title not in text_set:
-                text_set.add(title)
+            self.text_set.add(wiki_page.find('title').text.strip())
             
-            id = int(wiki_page.find('id').text.strip())
-            if id not in text_set:
-                text_set.add(id)
+            self.text_set.add(int(wiki_page.find('id').text.strip()))
 
-            text = wiki_page.find('text').text.strip()
-            if text not in text_set:
-                text_set.add(text)
+            self.text_set.add(wiki_page.find('text').text.strip())
+            
+    def tokenize(self, text_set : Set) -> None:
+        link_regex = '''\[\[[^\[]+?\]\]'''
+        text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+
+        for 
+        cool_token = re.findall(n_regex, "this is a cool string")
+
+
         
+    
