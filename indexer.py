4f3e88a79f2d1ab9e63a3af5217f76ca91287fbc
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 20:52:20 -0400

    Working on tokenizing

--- indexer.py
+++ indexer.py
@@ -1,22 +1,25 @@
 from posixpath import split
 import sys
 from tokenize import String
-from typing import Set
 import xml.etree.ElementTree as et
 import re
-
 from file_io import write_title_file
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
 
 class Indexer:
     def __init__(self) -> None:
         self.corpus = set()
+               
         self.id_to_title = {}
         self.link_to_links = {}
+
         if len(sys.argv) - 1 != 4:
             print("Fewer than four arguments!")
         else:
             self.parse(sys.argv[1])
             write_title_file(sys.argv[2], self.id_to_title)
+            self.tokenize()
 
     def parse(self, input_file : String) -> None:
         wiki_tree = et.parse(input_file)
@@ -30,14 +33,22 @@ class Indexer:
             self.corpus.add(wiki_page.find('text').text.strip().lower())
             
     def tokenize(self) -> None:
+        self.old_corpus = self.corpus 
+        self.corpus = set()
         link_regex = '''\[\[[^\[]+?\]\]'''
         text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
+        STOP_WORDS = set(stopwords.words('english'))
 
-        for string in self.corpus:
+        for string in self.old_corpus:
             text = re.findall(text_regex, string)
             links = re.findall(link_regex, string)
+            for word in text:
+                if word not in STOP_WORDS:
+                    stemmer = PorterStemmer(word)
+                    self.corpus.add(word)
+
             for link in links:
-                split_link = link.split('|')
+                split_link = str(link).split('|')
                 if len(split_link) == 1:
                     self.corpus.add(split_link[0])
                     self.link_to_links[split_link[0]] = []
@@ -46,7 +57,3 @@ class Indexer:
                     self.link_to_links[split_link[0]] = []
                 else:
                     break
-
-    def print_corpus(self):
-        for word in self.corpus:
-            print(word)
