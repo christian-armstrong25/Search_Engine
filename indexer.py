290081c9cda41ef3d7fe062b80c38df09be82321
christian-armstrong25 <christian_armstrong@brown.edu>
2022-04-24 01:07:53 -0400

    literally the cleanest freaking code I have ever seen in my life

--- indexer.py
+++ indexer.py
@@ -6,10 +6,11 @@ from file_io import write_title_file
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 
+
 class Indexer:
     def __init__(self) -> None:
         self.corpus = set()
-               
+
         self.ids_to_titles = {}
         self.link_to_title = {}
         self.words_to_doc_relevance = {}
@@ -20,7 +21,7 @@ class Indexer:
             self.parse(sys.argv[1])
             write_title_file(sys.argv[2], self.ids_to_titles)
 
-    def parse(self, input_file : String) -> None:
+    def parse(self, input_file: String) -> None:
         link_regex = '''\[\[[^\[]+?\]\]'''
         text_regex = '''[a-zA-Z0-9]+'[a-zA-Z0-9]+|[a-zA-Z0-9]+'''
         STOP_WORDS = set(stopwords.words('english'))
@@ -29,32 +30,46 @@ class Indexer:
         wiki_xml_root = wiki_tree.getroot()
 
         for wiki_page in wiki_xml_root:
-            for title_word in re.findall(text_regex, wiki_page.find('title').text.strip().lower()):
-                if title_word not in STOP_WORDS:
-                    self.corpus.add(stemmer.stem(title_word))
-            
-            self.ids_to_titles[int(wiki_page.find('id').text.strip())] = wiki_page.find('title').text.strip().lower()
-                
-            for text_word in re.findall(text_regex, wiki_page.find('text').text.strip().lower()):
-                if text_word not in STOP_WORDS:
-                    self.corpus.add(stemmer.stem(text_word))
+            page_id = int(wiki_page.find('id').text.strip())
+            self.word_count_in_page = {}
+            self.ids_to_titles[page_id] = wiki_page.find('title').text.strip()
+            words = [re.findall(text_regex, wiki_page.find(
+                'text').text.strip().lower())]
+            words.append(re.findall(text_regex, wiki_page.find(
+                'title').text.strip().lower()))
 
             for link in re.findall(link_regex, wiki_page.find('text').text):
                 split_link = str(link).strip().strip("[[]]").strip().split('|')
+
                 if len(split_link) == 1:
-                    self.corpus.add(split_link[0])
-                    if split_link[0] in self.link_to_title:
-                        list(self.link_to_title[split_link[0]]).append(wiki_page.find('title').text.strip())
-                    else:
-                        self.link_to_title[split_link[0]] = [wiki_page.find('title').text.strip()]
-                elif len(split_link) == 2:
-                    self.corpus.add(split_link[1])
-                    if split_link[0] in self.link_to_title:
-                        list(self.link_to_title[split_link[0]]).append(wiki_page.find('title').text.strip())
-                    else:
-                        self.link_to_title[split_link[0]] = [wiki_page.find('title').text.strip()]
+                    words.append(split_link[0])
                 else:
-                    break
-    
-    def term_frequency(self, input_file : String):
-        
+                    words.append(split_link[1])
+
+                if split_link[0] in self.link_to_title:
+                    list(self.link_to_title[split_link[0]]).append(
+                        wiki_page.find('title').text.strip())
+                else:
+                    self.link_to_title[split_link[0]] = [
+                        wiki_page.find('title').text.strip()]
+
+            for word in words:
+                if word not in STOP_WORDS:
+                    self.corpus.add(stemmer.stem(word))
+                    if word not in self.words_to_doc_relevance:
+                        self.words_to_doc_relevance[word] = {page_id: 1}
+                        self.word_count_in_page[word] = 1
+                    else:
+                        if int(wiki_page.find('id').text.strip()) not in self.words_to_doc_relevance[word]:
+                            self.words_to_doc_relevance[word][page_id] = 1
+                            self.word_count_in_page[word] = 1
+                        else:
+                            self.words_to_doc_relevance[word][page_id] += 1
+                            self.word_count_in_page[word] += 1
+
+            word_counts = self.word_count_in_page.values()
+            max_word_count_on_page = max(word_counts)
+
+            for word in self.words_to_doc_relevance:
+                if page_id in self.words_to_doc_relevance[word]:
+                    self.words_to_doc_relevance[word][page_id] /= max_word_count_on_page
--- test_search.py
+++ test_search.py
@@ -6,6 +6,4 @@ sys.argv = ["indexer.py", "wikis/CustomWiki.xml",
             "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
 # print(Indexer().corpus)
 # print(Indexer().id_to_title)
-print(Indexer().link_to_title)
-
-
+# print(Indexer().link_to_title)
