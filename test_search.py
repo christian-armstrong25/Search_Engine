c26c43712bc8c0e86a7c38f75500954243143c35
dantheman6383 <daniel_liu2@brown.edu>
2022-04-23 20:36:58 -0400

    Parsing DONE

--- test_search.py
+++ test_search.py
@@ -4,4 +4,5 @@ from query import *
 
 sys.argv = ["indexer.py", "wikis/CustomWiki.xml", "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
 print(Indexer().corpus)
+print(Indexer().id_to_title)
 
