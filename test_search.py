4bc412051f34b2ea976b327490e864e087a553b0
christian-armstrong25 <christian_armstrong@brown.edu>
2022-04-23 23:12:35 -0400

    hi

--- test_search.py
+++ test_search.py
@@ -2,7 +2,8 @@ from sys import argv
 from indexer import *
 from query import *
 
-sys.argv = ["indexer.py", "wikis/SmallWiki.xml", "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
-print(Indexer().corpus)
-print(Indexer().id_to_title)
+sys.argv = ["indexer.py", "wikis/SmallWiki.xml",
+            "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
+# print(Indexer().corpus)
+# print(Indexer().id_to_title)
 print(Indexer().link_to_title)
