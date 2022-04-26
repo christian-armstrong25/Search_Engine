fca5e5cea239bccf405ef471367d510be7ae1d6b
dantheman6383 <daniel_liu2@brown.edu>
2022-04-26 01:50:55 -0400

    hiiii

--- test_search.py
+++ test_search.py
@@ -1,9 +1,9 @@
 from sys import argv
-from indexer import *
+from index import *
 from query import *
 
-# sys.argv = ["indexer.py", "wikis/MedWiki.xml",
-#             "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
+sys.argv = ["index.py", "wikis/MedWiki.xml",
+            "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
 # print(Indexer().corpus)
 #print(Indexer().ids_to_titles)
 #print(Indexer().links_from_page)
