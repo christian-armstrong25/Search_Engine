abc77ab9e616005925e870fbf280e3b097af8ced
christian-armstrong25 <christian_armstrong@brown.edu>
2022-04-26 15:01:35 -0400

    djksl

--- test_search.py
+++ test_search.py
@@ -2,15 +2,14 @@ from sys import argv
 from index import *
 from query import *
 
-sys.argv = ["index.py", "wikis/MedWiki.xml",
+sys.argv = ["index.py", "wikis/PageRankExample3.xml",
             "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
-# print(Indexer().corpus)
 #print(Indexer().ids_to_titles)
 #print(Indexer().links_from_page)
 #print(Indexer().words_to_doc_relevance)
 #print(Indexer().weight_dictionary)
-#print(Indexer().ids_to_pageranks)
+print(Indexer().ids_to_pageranks)
 
-sys.argv = ["query.py", "--pagerank", "wikis/MedWiki.xml", 
-             "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
-print(Query().ids_to_relevance)
+# sys.argv = ["query.py", "--pagerank", "wikis/PageRankExample3.xml", 
+#              "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
+# print(Query().ids_to_relevance)
