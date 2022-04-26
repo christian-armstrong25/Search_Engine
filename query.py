f0e0aca5107d5c453ff9c41cbdb888f33cadd9ba
christian-armstrong25 <christian_armstrong@brown.edu>
2022-04-25 20:58:30 -0400

    jjiji

--- query.py
+++ query.py
@@ -13,7 +13,8 @@ class Query:
         self.words_to_doc_relevance = {}
         read_words_file(sys.argv[4], self.words_to_doc_relevance)
         self.query = []
-        self.ids_to_relevance = self.ids_to_titles
+        self.ids_to_relevance = dict.fromkeys(self.ids_to_titles, 0)
+
         while (True):
             self.input = input("Search: ")
             if self.input == ":quit":
@@ -23,19 +24,20 @@ class Query:
                 print(self.ids_to_relevance)
     
     def score_docs(self) -> None:
-        stemmer = PorterStemmer()
         self.query = self.input.split()
         for word in self.query:
-            if word not in STOP_WORDS:
-                word = stemmer.stem(word)
-        
-        for word in self.words_to_doc_relevance:
-            for id in self.words_to_doc_relevance[word]:
-                if isinstance(self.ids_to_relevance[id], str):
-                    self.ids_to_relevance[id] = self.words_to_doc_relevance[word][id]
-                else:
+            if word not in STOP_WORDS and word in self.words_to_doc_relevance:
+                word = PorterStemmer().stem(word)
+                for id in self.words_to_doc_relevance[word]:
                     self.ids_to_relevance[id] += self.words_to_doc_relevance[word][id]
-        self.ids_to_relevance = sorted(self.ids_to_relevance.items(), key= lambda x : x[1], reverse = True)
+
+
+
+
+
+
+
+            self.ids_to_relevance = sorted(self.ids_to_relevance.items(), key= lambda x : x[1], reverse = True)
 
         # for id in range(0, len(self.ids_to_relevance), 1):
         #     self.ids_to_relevance[id][1] = self.ids_to_titles[id]
