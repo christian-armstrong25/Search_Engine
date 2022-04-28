df2d6b6db9c1321374f9f847c1d76543e12e5cb1
dantheman6383 <daniel_liu2@brown.edu>
2022-04-28 02:05:59 -0400

    Need to make compatible with Gradescope

--- test_search.py
+++ test_search.py
@@ -2,19 +2,19 @@ from sys import argv
 from index import *
 from query import *
 
-sys.argv = ["python3", "index.py", "wikis/test_tf_idf.xml",
+sys.argv = ["python3", "index.py", "wikis/MedWiki.xml",
              "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
 # Indexer("wikis/MedWiki.xml", "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt")
 # # print(Indexer().ids_to_titles)
 # print(Indexer("wikis/HandoutWiki.xml",
 #               "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").ids_links_titles)
-print(Indexer("wikis/test_tf_idf.xml",
-            "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").words_to_doc_relevance)
+# print(Indexer("wikis/MedWiki.xml",
+#             "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").words_to_doc_relevance)
 # print(Indexer("wikis/MedWiki.xml",
 #              "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").weight_dictionary)
 # print(Indexer("wikis/MedWiki.xml",
 #              "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").ids_to_pageranks)
 
-# sys.argv = ["query.py", "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
-# print(Querier(True, "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt").words_to_doc_relevance)
+sys.argv = ["query.py", "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt"]
+Querier(False, "text_files/titles.txt", "text_files/docs.txt", "text_files/words.txt")
 
